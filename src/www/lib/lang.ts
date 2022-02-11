f9e3a176586359e95263aac086d56eededbb145f
Bolo <53911231+boloped@users.noreply.github.com>
2022-02-11 16:02:33 +0100

    Added German translation (#9)

--- src/www/lib/lang.ts
+++ src/www/lib/lang.ts
@@ -22,7 +22,18 @@ export const allLangs = {
 		UNKNOWN_ARTIST: "Artista sconosciuto",
 		UNKNOWN_TITLE: "Titolo sconosciuto",
 		LYRICS_COPYRIGHT: "Testi offerti da %PROVIDER%",
-	}
+	},
+	de: {
+		NOT_PLAYING: "Kein Titel wird abgespielt",
+		PLEASE_PLAY_SONG: "Bitte spiele etwas ab",
+		PLAYING_ON_APP: "Weiterspielen",
+		SCROBBLE_COUNT: "%COUNT% mal abgespielt",
+		NO_LYRICS: "Keine Songtexte verfügbar",
+		NOW_PLAYING_TITLE: "\"%TITLE%\" von %ARTIST%",
+		UNKNOWN_ARTIST: "Unbekannter Künstler",
+		UNKNOWN_TITLE: "Unbekannter Titel",
+		LYRICS_COPYRIGHT: "Songtexte bereitgestellt von %PROVIDER%",
+	},
 };
 
 export default (allLangs[config.language || navigator.language.split("-")[0]] || allLangs.en);
