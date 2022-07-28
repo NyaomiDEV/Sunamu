7448adb5018a0e723da69eae5ff576f71783ea81
Naomi Calabretta <me@arytonex.pw>
2022-07-28 07:36:17 +0200

    update ALL the deps

--- src/main/appStatus.ts
+++ src/main/appStatus.ts
@@ -6,7 +6,7 @@ export const argv = yargs(hideBin(process.argv)).argv;
 
 export const widgetModeElectron = checkFunctionality(getConfig("scenes").electron.widgetMode, "widget-electron");
 export const widgetMode = checkFunctionality(getConfig("scenes").default.widgetMode, "widget");
-export const debugMode = checkFunctionality(getConfig("debugMode"), "debug");
+export const debugMode = checkFunctionality(getConfig("debugMode"), "sunamu-debug");
 export const waylandOzone = checkFunctionality(getConfig("waylandOzone"), "wayland-ozone");
 
 export const useElectron = checkFunctionality(getConfig("useElectron"), "electron");
