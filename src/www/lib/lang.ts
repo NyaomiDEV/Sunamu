d70ff57651781934b44460f9f7a847ee3b86ed6b
Ernest Pérez <37526625+epcosta17@users.noreply.github.com>
2022-07-28 16:57:16 -0500

    Spanish translation added. (#22)

--- src/www/lib/lang.ts
+++ src/www/lib/lang.ts
@@ -62,6 +62,18 @@ export const allLangs: LanguageData = {
 		UNKNOWN_TITLE: "Onbekende titel",
 		LYRICS_COPYRIGHT: "Songtekst afkomstig van %PROVIDER%",
 	},
+  es: {
+			NOT_PLAYING: "No hay pistas reproduciéndose",
+			PLEASE_PLAY_SONG: "Por favor reproduce algo",
+			PLAYING_ON_APP: "Reproduciendo en",
+			SCROBBLE_COUNT: "Reproducido %COUNT% veces",
+      LOADING_LYRICS: "Cargando letras",
+			NO_LYRICS: "No hay letras disponibles",
+			NOW_PLAYING_TITLE: "\"%TITLE%\" de %ARTIST%",
+			UNKNOWN_ARTIST: "Artista Desconocido",
+			UNKNOWN_TITLE: "Título desconocido",
+			LYRICS_COPYRIGHT: "Letras proporcionadas por %PROVIDER%",
+  },
 };
 
 export default (allLangs[config.language || navigator.language.split("-")[0]] || allLangs.en);
