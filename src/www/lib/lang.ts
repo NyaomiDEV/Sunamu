576dcfd6066c4e477eafc196f7495792e66f117b
Ernest Pérez <37526625+epcosta17@users.noreply.github.com>
2022-07-28 11:43:34 -0500

    Spanish translation added

--- src/www/lib/lang.ts
+++ src/www/lib/lang.ts
@@ -34,6 +34,17 @@ export const allLangs = {
 		UNKNOWN_TITLE: "Unbekannter Titel",
 		LYRICS_COPYRIGHT: "Songtexte bereitgestellt von %PROVIDER%",
 	},
+	es: {
+			NOT_PLAYING: "No hay pistas reproduciéndose",
+			PLEASE_PLAY_SONG: "Por favor reproduce algo",
+			PLAYING_ON_APP: "Reproduciendo en",
+			SCROBBLE_COUNT: "Reproducido %COUNT% veces",
+			NO_LYRICS: "No hay letras disponibles",
+			NOW_PLAYING_TITLE: "\"%TITLE%\" de %ARTIST%",
+			UNKNOWN_ARTIST: "Artista Desconocido",
+			UNKNOWN_TITLE: "Título desconocido",
+			LYRICS_COPYRIGHT: "Letras proporcionadas por %PROVIDER%",
+	},
 };
 
 export default (allLangs[config.language || navigator.language.split("-")[0]] || allLangs.en);
