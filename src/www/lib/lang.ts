319bc941919fc1f920b631051ef06191221ac331
kamiya10 <58339640+kamiya10@users.noreply.github.com>
2022-07-29 18:19:36 +0800

    Update lang.ts

--- src/www/lib/lang.ts
+++ src/www/lib/lang.ts
@@ -78,7 +78,7 @@ export const allLangs: LanguageData = {
 		NOT_PLAYING: "目前沒有正在播放的音樂",
 		PLEASE_PLAY_SONG: "請播放一些音樂",
 		PLAYING_ON_APP: "在",
-		SCROBBLE_COUNT: "Scrobbled %COUNT% times",
+		SCROBBLE_COUNT: "已播放 %COUNT% 次",
 		LOADING_LYRICS: "讀取歌詞中",
 		NO_LYRICS: "沒有可用的歌詞",
 		NOW_PLAYING_TITLE: "\"%TITLE%\" - %ARTIST%",
