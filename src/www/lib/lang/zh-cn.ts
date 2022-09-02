24965d42c1f445423a41792be8e6bd40d97167a9
Aimer Neige <aimer.neige.soft@gmail.com>
2022-09-02 14:23:20 +0800

    Update zh-cn Translation

---
+++ src/www/lib/lang/zh-cn.ts
@@ -0,0 +1,15 @@
+import { Language } from "../../../types";
+
+export default {
+	NOT_PLAYING: "目前没有正在播放的音乐",
+	PLEASE_PLAY_SONG: "请播放一些音乐",
+	PLAYING_ON_APP: "在",
+	PLAY_COUNT: "已播放 %COUNT% 次",
+	SCROBBLE_COUNT: "%COUNT% scrobbles", // If there's a localized version, use it (Scrobbles as in Last.FM)
+	LOADING_LYRICS: "读取歌词中",
+	NO_LYRICS: "没有可用的歌词",
+	NOW_PLAYING_TITLE: "\"%TITLE%\" - %ARTIST%",
+	UNKNOWN_ARTIST: "未知艺术家",
+	UNKNOWN_TITLE: "未知标题",
+	LYRICS_COPYRIGHT: "歌词提供：%PROVIDER%",
+} as Language;
