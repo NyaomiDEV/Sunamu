6ac11679e221270bcf28693f34f1c72f112c7130
Fjuro <ifjuro@proton.me>
2022-09-10 21:38:42 +0200

    Create cs.ts

---
+++ src/www/lib/lang/cs.ts
@@ -0,0 +1,15 @@
+import { Language } from "../../../types";
+
+export default {
+	NOT_PLAYING: "Nehraje",
+	PLEASE_PLAY_SONG: "Přehraj prosím něco",
+	PLAYING_ON_APP: "Hraje na",
+	PLAY_COUNT: "Přehráno %COUNT%krát",
+	SCROBBLE_COUNT: "%COUNT% scrobblů",
+	LOADING_LYRICS: "Načítání textů",
+	NO_LYRICS: "Nejsou dostupné žádné texty",
+	NOW_PLAYING_TITLE: "\"%TITLE%\" od %ARTIST%",
+	UNKNOWN_ARTIST: "Neznámý umělec",
+	UNKNOWN_TITLE: "Neznámý název",
+	LYRICS_COPYRIGHT: "Texty poskytuje %PROVIDER%",
+} as Language;
